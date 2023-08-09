6763add175d6a738a8e1b4b6bfb34aa4164c496f
Benito J. Gonzalez <bgonzalez@unicon.net>
2023-08-08 19:04:32 -0700

    chore: split layout into head, header and footer functions

--- src/clj/upwebsite/layout.clj
+++ src/clj/upwebsite/layout.clj
@@ -3,22 +3,35 @@
             [optimus.link :as link]
             [stasis.core :as stasis]))
 
+(declare layout-page-head)
+(declare layout-page-header)
+(declare layout-page-footer)
+
 (defn layout-page [request page]
   (html5
-   [:head
-    [:meta {:charset "utf-8"}]
-    [:meta {:name "viewport"
-            :content "width=device-width, initial-scale=1.0"}]
-    [:title "Tech blog"]
-    [:link {:rel "stylesheet" :href "/styles/main.css"}]
-    [:link {:rel "stylesheet" :href "/styles/animate.css"}]
-    [:link {:rel "stylesheet" :href "/styles/bootstrap.min.css"}]
-    [:link {:rel "stylesheet" :href "/styles/nivo-lightbox.css"}]
-    [:link {:rel "stylesheet" :href "/styles/responsive.css"}]
-    [:link {:rel "stylesheet" :href (link/file-path request "/styles/slicknav.css")}]
-	[:link {:rel "stylesheet" :href (link/file-path request "/pygments-css/autumn.css")}]
-	]
+    (layout-page-head request page)
     [:body
-     [:div.logo "cjohansen.no"]
-     [:div.body page]]))
- 
+     (layout-page-header request page)
+     page
+     (layout-page-footer request page)]))
+
+(defn layout-page-head [request page]
+  (html5
+    [:head
+     [:meta {:charset "utf-8"}]
+     [:meta {:name "viewport"
+             :content "width=device-width, initial-scale=1.0"}]
+     [:title "uPortal"]
+     [:link {:rel "stylesheet" :href (link/file-path request "/styles/main.css")}]
+     [:link {:rel "stylesheet" :href (link/file-path request "/styles/animate.css")}]
+     [:link {:rel "stylesheet" :href (link/file-path request "/styles/bootstrap.min.css")}]
+     [:link {:rel "stylesheet" :href (link/file-path request "/styles/nivo-lightbox.css")}]
+     [:link {:rel "stylesheet" :href (link/file-path request "/styles/responsive.css")}]
+     [:link {:rel "stylesheet" :href (link/file-path request "/styles/slicknav.css")}]
+     [:link {:rel "stylesheet" :href (link/file-path request "/pygments-css/autumn.css")}]
+     ]))
+
+(defn layout-page-header [request page]
+  )
+
+(defn layout-page-footer [request page])
