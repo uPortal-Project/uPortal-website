ff912bdadc9254dec797d677a926ded39f7f1b24
Benito J. Gonzalez <bgonzalez@unicon.net>
2023-08-31 17:50:37 -0700

    fix: customize save-assets to ignore context-path

--- src/clj/upwebsite/static.clj
+++ src/clj/upwebsite/static.clj
@@ -1,19 +1,27 @@
 (ns upwebsite.static
   (:require
    [stasis.core :as stasis]
-   [optimus.export]
+   [optimus.export :as ex]
    [optimus.optimizations :as optimizations]
    [taoensso.timbre :as log]
    [upwebsite.web :as web]))
 
 (def export-dir "dist")
 
+(defn save-assets
+  "Replace optimus save-assets fn to ignore context-path when saving assets."
+  [assets target-dir]
+  (doseq [asset assets]
+    (let [path (str target-dir (:path asset))]
+      (#'ex/create-folders path)
+      (#'ex/save-asset-to-path asset path))))
+
 (defn export! [& [ctx-path]]
   (if ctx-path (reset! web/context-path ctx-path))
   (log/debug "context-path: " @web/context-path)
   (let [assets (optimizations/all (web/get-assets) {})]
     (stasis/empty-directory! export-dir)
-    (optimus.export/save-assets assets export-dir)
+    (save-assets assets export-dir)
     (stasis/export-pages (web/get-pages) export-dir {:optimus-assets assets})))
 
 (def -main export!)
