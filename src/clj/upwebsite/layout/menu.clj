5d9292446addbcc0991d3f62565f72b94cb180dd
Benito J. Gonzalez <bgonzalez@unicon.net>
2023-09-03 16:13:04 -0700

    chore: separate out menu data

---
+++ src/clj/upwebsite/layout/menu.clj
@@ -0,0 +1,31 @@
+(ns upwebsite.layout.menu
+  "Menu links")
+
+(def features ["/features/index.html"
+               "/features/case-studies.html"
+               "/features/showcases.html"])
+
+(def support ["/support/index.html"
+              "/support/user-guide.html"
+              "/support/admin-guide.html"
+              "/support/developer-guide.html"
+              "/support/deployment-guide.html"
+              "/support/paid-support.html"])
+
+(def community ["/community/index.html"
+                "/community/roadmap.html"
+                "/community/mailing-lists.html"
+                "/community/code-of-conduct.html"
+                "/community/committers.html"
+                "/community/contributor-onboarding.html"])
+
+(def events ["/events/index.html"
+             "/events/dev-days.html"
+             "/events/monthly-calls.html"])
+
+(def about ["/about/index.html"
+            "/about/apereo.html"
+            "/about/governance.html"
+            "/about/partners.html"
+            "/about/news.html"
+            "/about/data-privacy-policy.html"])
